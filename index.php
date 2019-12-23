114377340433ef8a51f9bd9a48793c4de5854166
Made Saguna <made.saguna@noreply.github.com>
2019-12-23 10:50:27 +0800

    test index

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>LIFF Starter</title>
+        <title>Cek Pemilih</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
